b61d70cff98fe814020006e936a009ce3bc7161d
GrantBirki <grantbirki@github.com>
2024-07-05 13:14:13 -0700

    test for only required ci checks in the status function

--- __test__/functions/status.test.js
+++ __test__/functions/status.test.js
@@ -91,4 +91,57 @@ describe('status function', () => {
       expect.stringContaining('statusResult:')
     )
   })
+
+  test('should successfully get the status of a PR that is approved and in a cleanly mergeable state but only required CI checks are evaluated', async () => {
+    data.checks = 'required'
+
+    octokit.graphql = jest.fn().mockReturnValue({
+      repository: {
+        pullRequest: {
+          reviewDecision: 'APPROVED',
+          mergeStateStatus: 'CLEAN',
+          reviews: {
+            totalCount: 1
+          },
+          commits: {
+            nodes: [
+              {
+                commit: {
+                  checkSuites: {
+                    totalCount: 3
+                  },
+                  statusCheckRollup: {
+                    state: 'FAILURE',
+                    contexts: {
+                      nodes: [
+                        {
+                          isRequired: true,
+                          conclusion: 'SUCCESS'
+                        },
+                        {
+                          isRequired: true,
+                          conclusion: 'SKIPPED'
+                        },
+                        {
+                          isRequired: false,
+                          conclusion: 'FAILURE'
+                        }
+                      ]
+                    }
+                  }
+                }
+              }
+            ]
+          }
+        }
+      }
+    })
+
+    expect(await status(octokit, context, prNumber, data)).toStrictEqual({
+      review_decision: 'APPROVED',
+      merge_state_status: 'CLEAN',
+      total_approvals: 1,
+      commit_status: 'SUCCESS'
+    })
+  })
 })
