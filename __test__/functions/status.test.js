9939b709db95e764e29fcded37b0034827ebbbb3
GrantBirki <grantbirki@github.com>
2024-07-05 13:08:37 -0700

    adding status tests

--- __test__/functions/status.test.js
+++ __test__/functions/status.test.js
@@ -5,21 +5,86 @@ jest.mock('@actions/core')
 // Get references to the mocked functions
 const core = require('@actions/core')
 
+const prNumber = 123
+
+var data
+var context
+var octokit
+var graphQLOK
+
 describe('status function', () => {
   beforeEach(() => {
     // Clear all mocks before each test
     jest.clearAllMocks()
+
+    data = {
+      checks: 'all'
+    }
+
+    context = {
+      actor: 'monalisa',
+      repo: {
+        owner: 'corp',
+        repo: 'test'
+      },
+      issue: {
+        number: 123
+      }
+    }
+
+    graphQLOK = jest.fn().mockReturnValue({
+      repository: {
+        pullRequest: {
+          reviewDecision: 'APPROVED',
+          mergeStateStatus: 'CLEAN',
+          reviews: {
+            totalCount: 1
+          },
+          commits: {
+            nodes: [
+              {
+                commit: {
+                  checkSuites: {
+                    totalCount: 3
+                  },
+                  statusCheckRollup: {
+                    state: 'SUCCESS',
+                    contexts: {
+                      nodes: [
+                        {
+                          isRequired: true,
+                          conclusion: 'SUCCESS'
+                        },
+                        {
+                          isRequired: true,
+                          conclusion: 'SKIPPED'
+                        },
+                        {
+                          isRequired: false,
+                          conclusion: 'SUCCESS'
+                        }
+                      ]
+                    }
+                  }
+                }
+              }
+            ]
+          }
+        }
+      }
+    })
+
+    octokit = {
+      graphql: graphQLOK
+    }
   })
 
   test('should successfully get the status of a PR that is approved, with CI passing, and in a cleanly mergeable state', async () => {
-    // const status = {
-    //   review_decision: 'APPROVED',
-    //   total_approvals: 2,
-    //   merge_state_status: 'CLEAN',
-    //   commit_status: 'SUCCESS'
-    // }
-    // const result = await status(status, data)
-    // expect(core.debug).toHaveBeenCalledWith(expect.stringContaining('statusResult'))
-    // expect(result).toBe(true)
+    expect(await status(octokit, context, prNumber, data)).toStrictEqual({
+      review_decision: 'APPROVED',
+      merge_state_status: 'CLEAN',
+      total_approvals: 1,
+      commit_status: 'SUCCESS'
+    })
   })
 })
