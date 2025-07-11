7300acb7245e0171134ee965ded611d676cfd71a
GrantBirki <grant.birkinbine@gmail.com>
2025-07-10 22:33:32 -0700

    Update coverage badge to reflect current test coverage and bump version to v2.0.0

--- dist/index.js
+++ dist/index.js
@@ -32329,7 +32329,7 @@ if (process.env['CI'] === 'true' && process.env['JEST_TEST'] !== 'true') {
 
 Object.defineProperty(exports, "__esModule", ({ value: true }));
 exports.VERSION = void 0;
-exports.VERSION = 'v1.5.0';
+exports.VERSION = 'v2.0.0';
 
 
 /***/ }),
